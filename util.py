388b3a624aac978a0709bc5a1f0956aa3121397b
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2021-04-02 15:02:53 +0200

    List movies objects is done

--- util.py
+++ util.py
@@ -4,6 +4,7 @@ def request(base_url, *args):
     url = base_url + "?"
     for arg in args:
         url = url + arg + "&"
-    return requests.get(url).json()
+    reponse = requests.get(url)
+    return reponse.json() 
      
 
--- webapi.py
+++ webapi.py
@@ -4,11 +4,11 @@ BASE_API="https://yts.mx/api/v2/"
 
 def main():
     print("Testing start")
-    print(movie_details("10"))
+    print(list_movies(query_term="The Lost World: Jurassic Park").items())
     print("Testing end")
 
 # List Movies - https://yts.mx/api#list_movies
-def list_movies(limit="20", page="0", quality="1080p", minimum_rating="0", query_term="0", genre="All", sort_by="date_added", order_by="desc", with_rt_ratings="false"):
+def list_movies(limit="20", page="1", quality="1080p", minimum_rating="0", query_term="0", genre="all", sort_by="date_added", order_by="desc", with_rt_ratings="false"):
     return util.request(BASE_API+"list_movies.json", "limit=" + str(limit), "page=" + str(page), "quality" + str(quality), "minimum_rating" + str(minimum_rating), "query_term=" + str(query_term), "genre=" + str(genre), "sort_by" + str(sort_by), "order_by=" + str(order_by), "with_rt_ratings=" + str(with_rt_ratings))
 
 # Movie Details - https://yts.mx/api#movie_details
